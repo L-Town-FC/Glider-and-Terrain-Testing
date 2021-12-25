5f5d9fa966a39aa42bdeb0f14a42264552821ca9
ColinKrohto <7willythekid@gmail.com>
2021-12-25 11:55:13 -0500

    Added shader to terrain
    also moved Noise generator into its own function

--- Assets/Scripts/MeshGenerator.cs
+++ Assets/Scripts/MeshGenerator.cs
@@ -9,21 +9,22 @@ public class MeshGenerator : MonoBehaviour
 
     Vector3[] vertices;
     int[] triangles;
+    Color[] colors;
 
     public int xSize = 20;
     public int zSize = 20;
 
     [Range(0,0.1f)]
-    public float coarseScale1 = 10f;
+    public float coarseScale1 = 0f;
     [Range(0, 0.1f)]
-    public float coarseScale2 = 10f;
+    public float coarseScale2 = 0f;
 
     [Range(0,10)]
     public float coarseRange = 2f;
     
-    [Range(0,0.5f)]
+    [Range(0,1f)]
     public float fineScale1 = 1f;
-    [Range(0, 0.5f)]
+    [Range(0, 1f)]
     public float fineScale2 = 1f;
 
     [Range(0,2)]
@@ -31,6 +32,11 @@ public class MeshGenerator : MonoBehaviour
 
     public float xOffset;
     public float yOffset;
+
+    public Gradient gradient;
+
+    float minTerrainHeight;
+    float maxTerrainHeight;
 
     public void GenerateMap() //function used by editor
     {
@@ -62,14 +68,16 @@ public class MeshGenerator : MonoBehaviour
         for(int i = 0, z = 0; z <= zSize; z++)
         {
             for(int x = 0; x <= xSize; x++)
-            {
-                //scales zoom the noise in and out/range increase max and min value of noise
-                float coarseY1 = Mathf.PerlinNoise(x * coarseScale1 + xOffset, z * coarseScale1 + yOffset) * coarseRange;
-                float coarseY2 = Mathf.PerlinNoise(x * coarseScale2 + xOffset, z * coarseScale2 + yOffset) * coarseRange;
-                float fineY1 = Mathf.PerlinNoise(x * fineScale1, z * fineScale1) * fineRange;
-                float fineY2 = Mathf.PerlinNoise(x * fineScale2, z * fineScale2) * fineRange;
+            {                
+                float y = NoiseGenerator(x, z);
 
-                float y = coarseY1 + coarseY2 + fineY1 + fineY2;
+                if(y > maxTerrainHeight)
+                {
+                    maxTerrainHeight = y;
+                }
+                if (y < minTerrainHeight) {
+                    minTerrainHeight = y;
+                }
 
                 vertices[i] = new Vector3(x, y, z);
                 i++;
@@ -85,6 +93,7 @@ public class MeshGenerator : MonoBehaviour
         {
             for (int x = 0; x < xSize; x++) //loops over row and makes 2 sets of triangles(1 quad) out of vertices
             {
+                //move this into own function
                 triangles[tris + 0] = vert + 0;
                 triangles[tris + 1] = vert + xSize + 1;
                 triangles[tris + 2] = vert + 1;
@@ -98,7 +107,18 @@ public class MeshGenerator : MonoBehaviour
 
             vert++; //need to increment when a row is done or else a triangle is created between end of row and start of new row
         }
-  
+
+        colors = new Color[vertices.Length];
+        for (int i = 0, z = 0; z <= zSize; z++)
+        {
+            for (int x = 0; x <= xSize; x++)
+            {
+                float height = Mathf.InverseLerp(minTerrainHeight, maxTerrainHeight,vertices[i].y); //normalizes height value  between 0 and 1 so color gradient works properly
+                colors[i] = gradient.Evaluate(height);
+                i++;
+            }
+        }
+
     }
 
     void UpdateMesh() //makes sure mesh is set up correctly and points arent saved from previous mesh
@@ -107,7 +127,20 @@ public class MeshGenerator : MonoBehaviour
 
         mesh.vertices = vertices;
         mesh.triangles = triangles;
+        mesh.colors = colors;
 
         mesh.RecalculateNormals(); //fixes lighting of mesh based on surface normals
+    }
+
+    float NoiseGenerator(float x, float z)
+    {
+        //scales zoom the noise in and out/range increase max and min value of noise
+        float coarseY1 = Mathf.PerlinNoise(x * coarseScale1 + xOffset, z * coarseScale1 + yOffset) * coarseRange;
+        float coarseY2 = Mathf.PerlinNoise(x * coarseScale2 + xOffset, z * coarseScale2 + yOffset) * coarseRange;
+        float fineY1 = Mathf.PerlinNoise(x * fineScale1, z * fineScale1) * fineRange;
+        float fineY2 = Mathf.PerlinNoise(x * fineScale2, z * fineScale2) * fineRange;
+
+        return coarseY1 + coarseY2 + fineY1 + fineY2;
+
     }
 }
