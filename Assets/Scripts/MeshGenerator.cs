c54e2bb86d1ffe4901c3b6ec445e44505f8c3750
ColinKrohto <7willythekid@gmail.com>
2021-12-27 12:35:32 -0500

    Continued tweaking noise generator

--- Assets/Scripts/MeshGenerator.cs
+++ Assets/Scripts/MeshGenerator.cs
@@ -14,24 +14,35 @@ public class MeshGenerator : MonoBehaviour
     public int xSize = 20;
     public int zSize = 20;
 
-    [Range(0,0.1f)]
-    public float coarseScale1 = 0f;
-    [Range(0, 0.1f)]
-    public float coarseScale2 = 0f;
+    //[Range(0,0.1f)]
+    //public float coarseFrequency1 = 0f;
+    //[Range(0, 0.1f)]
+    //public float coarseFrequency2 = 0f;
+
+    //[Range(0,10)]
+    //public float coarseAmplitude = 2f;
+    
+    //[Range(0,1f)]
+    //public float fineFrequency1 = 1f;
+    //[Range(0, 1f)]
+    //public float fineFrequency2 = 1f;
+
+    //[Range(0,2)]
+    //public float fineAmplitude = 0.5f;
 
     [Range(0,10)]
-    public float coarseRange = 2f;
-    
-    [Range(0,1f)]
-    public float fineScale1 = 1f;
-    [Range(0, 1f)]
-    public float fineScale2 = 1f;
+    public float amplitude = 1f;
 
-    [Range(0,2)]
-    public float fineRange = 0.5f;
+    public float startingFrequency = 0.1f;
+    float frequency;
+
+    public int octaves;
+    public float persistance;
+    [Range(0,1)]
+    public float lacunarity;
 
     public float xOffset;
-    public float yOffset;
+    public float zOffset;
 
     public Gradient gradient;
 
@@ -40,7 +51,9 @@ public class MeshGenerator : MonoBehaviour
 
     public void GenerateMap() //function used by editor
     {
-       
+        frequency = startingFrequency;
+        minTerrainHeight = 0;
+        maxTerrainHeight = 0;
         mesh = new Mesh();
         GetComponent<MeshFilter>().mesh = mesh;
 
@@ -65,50 +78,59 @@ public class MeshGenerator : MonoBehaviour
 
 
         //creates a list of vertices for the specified length(xSize) and width(zSize)
-        for(int i = 0, z = 0; z <= zSize; z++)
+        for (int j = 0; j < octaves; j++)
         {
-            for(int x = 0; x <= xSize; x++)
-            {                
-                float y = NoiseGenerator(x, z);
-
-                if(y > maxTerrainHeight)
-                {
-                    maxTerrainHeight = y;
-                }
-                if (y < minTerrainHeight) {
-                    minTerrainHeight = y;
-                }
-
-                vertices[i] = new Vector3(x, y, z);
-                i++;
-            }
-        }
-
-        triangles = new int[xSize * zSize * 6]; //generates array of of correct size based on length and widht
-
-        int vert = 0;
-        int tris = 0;
-
-        for (int z = 0; z < zSize; z++) //when a row is complete the new row is started
-        {
-            for (int x = 0; x < xSize; x++) //loops over row and makes 2 sets of triangles(1 quad) out of vertices
+            for (int i = 0, z = 0; z <= zSize; z++)
             {
-                //move this into own function
-                triangles[tris + 0] = vert + 0;
-                triangles[tris + 1] = vert + xSize + 1;
-                triangles[tris + 2] = vert + 1;
-                triangles[tris + 3] = vert + 1;
-                triangles[tris + 4] = vert + xSize + 1;
-                triangles[tris + 5] = vert + xSize + 2;
+                for (int x = 0; x <= xSize; x++)
+                {   
+                    float y = NoiseGenerator(x, z, j);
 
-                vert++; //shifts points over for next triangle
-                tris += 6; //when a quad is done, incremented to work on next quad
+                    if(float.IsInfinity(y) == true)
+                    {
+                        print("Is Infinite");
+                    } 
+                    //if (y > maxTerrainHeight)
+                    //{
+                    //    maxTerrainHeight = y;
+                    //}
+                    //if (y < minTerrainHeight) {
+                    //    minTerrainHeight = y;
+                    //}
+
+                    if (vertices[i] == Vector3.zero)
+                    {
+                        vertices[i] = new Vector3(x, y, z);
+                    }
+                    else
+                    {
+                        vertices[i] += new Vector3(0, y, 0);
+                    }
+                    i++;
+                }
             }
-
-            vert++; //need to increment when a row is done or else a triangle is created between end of row and start of new row
         }
+
+        for (int i = 0; i < vertices.Length; i++)
+        {
+            
+            float y = vertices[i].y;
+            if (y > maxTerrainHeight)
+            {
+                maxTerrainHeight = y;
+            }
+            if (y < minTerrainHeight)
+            {
+                minTerrainHeight = y;
+            }
+        }
+
+        print(new Vector2(minTerrainHeight, maxTerrainHeight));
+
+        TrianglesAndVertices();
 
         colors = new Color[vertices.Length];
+
         for (int i = 0, z = 0; z <= zSize; z++)
         {
             for (int x = 0; x <= xSize; x++)
@@ -132,15 +154,44 @@ public class MeshGenerator : MonoBehaviour
         mesh.RecalculateNormals(); //fixes lighting of mesh based on surface normals
     }
 
-    float NoiseGenerator(float x, float z)
+    float NoiseGenerator(float x, float z, int octave)
     {
-        //scales zoom the noise in and out/range increase max and min value of noise
-        float coarseY1 = Mathf.PerlinNoise(x * coarseScale1 + xOffset, z * coarseScale1 + yOffset) * coarseRange;
-        float coarseY2 = Mathf.PerlinNoise(x * coarseScale2 + xOffset, z * coarseScale2 + yOffset) * coarseRange;
-        float fineY1 = Mathf.PerlinNoise(x * fineScale1, z * fineScale1) * fineRange;
-        float fineY2 = Mathf.PerlinNoise(x * fineScale2, z * fineScale2) * fineRange;
+        frequency = startingFrequency;
+        frequency *= lacunarity * octave;
 
-        return coarseY1 + coarseY2 + fineY1 + fineY2;
+        //Frequencys zoom the noise in and out/Amplitude increase max and min value of noise
+        float perlinValue = Mathf.PerlinNoise((float)x * frequency + xOffset, (float)z * frequency + zOffset);
+        float noiseHeight = perlinValue * amplitude;
+        noiseHeight *= persistance * octave;
+        //frequency *= lacunarity * octave;
 
+        return noiseHeight;
+    }
+
+    void TrianglesAndVertices()
+    {
+        triangles = new int[xSize * zSize * 6]; //generates array of of correct size based on length and widht
+
+        int vert = 0;
+        int tris = 0;
+
+        for (int z = 0; z < zSize; z++) //when a row is complete the new row is started
+        {
+            for (int x = 0; x < xSize; x++) //loops over row and makes 2 sets of triangles(1 quad) out of vertices
+            {
+                //move this into own function
+                triangles[tris + 0] = vert + 0;
+                triangles[tris + 1] = vert + xSize + 1;
+                triangles[tris + 2] = vert + 1;
+                triangles[tris + 3] = vert + 1;
+                triangles[tris + 4] = vert + xSize + 1;
+                triangles[tris + 5] = vert + xSize + 2;
+
+                vert++; //shifts points over for next triangle
+                tris += 6; //when a quad is done, incremented to work on next quad
+            }
+
+            vert++; //need to increment when a row is done or else a triangle is created between end of row and start of new row
+        }
     }
 }
